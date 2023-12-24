b094df606b7f638555a1c2919694b02a2a60da04
Kanae Cho <y@oky.services>
2023-12-24 21:38:57 +0800

    initialized

---
+++ src/main/java/com/se2102/app/SpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.se2102.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/se2102/app/SpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2102.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
